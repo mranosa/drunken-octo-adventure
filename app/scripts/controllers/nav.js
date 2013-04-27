b4a7e60bcf96a17c2e8bf54c8b0169bc6b89587e
mranosa <mark.ranosa@gmail.com>
2013-04-27 15:06:25 +0800

    Initial nav controller implementation

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,9 @@
+'use strict';
+
+drunkenOctoAdventureApp.controller('NavCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
