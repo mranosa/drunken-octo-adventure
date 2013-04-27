e532adc16e14806ec4c792807796bf455c1aa72f
mranosa <mark.ranosa@gmail.com>
2013-04-27 15:10:50 +0800

    Added in inital controller logic

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,9 +1,31 @@
 'use strict';
 
-drunkenOctoAdventureApp.controller('NavCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
+drunkenOctoAdventureApp.controller('NavCtrl', [ '$location', '$scope', 
+	function($location, $scope) {
+
+  $scope.navs = [
+  	{
+  		name: 'item',
+  		url: '#/item'
+  	}, {
+  		name: 'pitch',
+  		url: '#/pitch'
+    }
   ];
-});
+
+  $scope.updateActiveNav = function(){
+    var navs = _.map($scope.navs, 
+      function(nav){ 
+        nav.navCls = '';
+        if($location.path() !== '/' && nav.url.indexOf($location.path()) === 1) {
+          nav.navCls = 'active';
+        }
+
+        return nav; 
+      }
+    );
+  };
+
+  $scope.$on('update_active_nav', $scope.updateActiveNav);
+
+}]);
