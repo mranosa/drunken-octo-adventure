125bfda8b86df21b0b3660b0966a721761045978
mranosa <mark.ranosa@gmail.com>
2013-04-27 14:50:25 +0800

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var drunkenOctoAdventureApp = angular.module('drunkenOctoAdventureApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
